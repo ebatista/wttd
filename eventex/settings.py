967fe35e47294b6a27926256ebe1ebaa8bffecfe
Eduardo Batista <ebatista@gmail.com>
2012-09-27 11:41:41 -0300

    Desativando o Debug e acertando paginas 404 e 500

--- eventex/settings.py
+++ eventex/settings.py
@@ -5,8 +5,8 @@ from unipath import Path
 PROJECT_DIR = Path(__file__).parent
 
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG 
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
--- eventex/urls.py
+++ eventex/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -15,4 +17,9 @@ urlpatterns = patterns('eventex.core.views',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+)
+
+if settings.DEBUG is False:
+    urlpatterns += patterns('',
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
