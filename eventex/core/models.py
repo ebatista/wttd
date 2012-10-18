00ec5076e11fe591cedae6c35d818c2b3cee46b0
Eduardo Batista <ebatista@gmail.com>
2012-10-18 12:51:14 -0300

    Talk

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from eventex.core.managers import KindContactManager, PeriodManager
 
 class Speaker(models.Model):
     name = models.CharField(_('Nome'), max_length=255) 
@@ -10,6 +11,10 @@ class Speaker(models.Model):
 
     def __unicode__(self):
         return self.name
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('core:speaker_detail', (), {'slug': self.slug})
 
 class Contact(models.Model):
     KINDS = (
@@ -21,5 +26,32 @@ class Contact(models.Model):
     kind = models.CharField(_('Tipo'), max_length=1, choices=KINDS)
     value = models.CharField(_('Valor'), max_length=255)
 
+
+    objects = models.Manager()
+    emails = KindContactManager('E')
+    phones = KindContactManager('P')
+    faxes = KindContactManager('F')
+
     def __unicode__(self):
         return self.value
+
+
+class Talk(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    start_time = models.TimeField(blank=True)
+    speakers = models.ManyToManyField('Speaker', verbose_name=_('palestrante'))
+
+    objects = PeriodManager()
+
+    class Meta:
+        verbose_name = _('Palestra')
+        verbose_name_plural = _('Palestras')
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return '/palestras/%d/' % self.pk
+
+
